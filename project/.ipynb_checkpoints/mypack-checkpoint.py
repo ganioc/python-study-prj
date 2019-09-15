c1cd0dac792e40c9962b03f8b2a501760cac26c6
yango3 <o3.yang@hotmail.com>
2019-09-15 22:18:48 +0800

    Update ipython

---
+++ project/.ipynb_checkpoints/mypack-checkpoint.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def SayHello(Name):
+    print("Hello ", Name)
+    return
+def SayGoodbye(Name):
+    print("Goodbye ", Name)
+    return
+
+
+# In[ ]:
+
+
+
+
---
+++ project/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,3 @@
+import os
+print("HELLO")
+os._exit(5)
