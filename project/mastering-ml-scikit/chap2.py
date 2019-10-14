5ad353f21c00dd4b4db65382f986e897d16511e6
yango3 <o3.yang@hotmail.com>
2019-10-14 14:55:59 +0800

    Update chap2 linear regression approximation

--- project/mastering-ml-scikit/chap2.py
+++ project/mastering-ml-scikit/chap2.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-X = np.array([[6],[8],[10], [14], [18]]).reshape(-1, 1)
+X_train = np.array([[6],[8],[10], [14], [18]]).reshape(-1, 1)
+
+Y_train = [ 7,9,13,17.5, 18]
+
+X_test = np.array([8,9,11,16,12]).reshape(-1,1)
+Y_test = [11,8.5, 15,18,11]
+
+model = LinearRegression()
+model.fit(X_train, Y_train)
+r_squared = model.score(X_test, Y_test)
+print(r_squared)
 
 
---
+++ project/mastering-ml-scikit/session2.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import numpy as np
+X = np.array([[6],[8], [10], [14], [18]]).reshape(-1,1)
+x_bar = X.mean()
+print(x_bar)
+variance = ((X - x_bar)**2).sum()/(X.shape[0] -1)
+print(variance)
+print(np.var(X, ddof=1)
+
+
+
+
+
+
+
+)
