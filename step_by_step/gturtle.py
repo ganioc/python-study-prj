45a0687a83b7cc0762f6685bfe2de545cb8ade9d
yango <o3.yang@hotmail.com>
2024-01-09 08:50:14 +0800

    add turtle.py

---
+++ step_by_step/gturtle.py
@@ -0,0 +1,18 @@
+import turtle
+
+screen = turtle.Screen()
+turtle = turtle.Turtle()
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+input("Press any key to exit ...")
+
+# screen.mainloop()
+
+# turtle.done()
